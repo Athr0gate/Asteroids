32999103b1fae637d5310ecb6278fa05985225cf
Athr0gate <garrett_hvacr@yahoo.com>
2024-10-15 23:27:03 -0400

    player class and methods

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     print("Starting asteroids!")
@@ -7,6 +8,11 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     if not pygame.get_init:
         pygame.init
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     running = True
     while running:
@@ -14,8 +20,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
-
+        frames = clock.tick(60)
+        dt = frames / 1000
+        
+       
 if __name__== "__main__":
     main()
